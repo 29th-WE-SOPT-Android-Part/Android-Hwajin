fc36c92ba1be2524c0df7cf5facd2ec11f51907c
hjh1161514 <hjh1161514@naver.com>
2021-10-29 23:04:16 +0900

    [FEAT] Assignment Week2

---
+++ Android_Seminar_Week3/app/src/main/java/com/example/android_seminar_week3/SignInActivity.kt
@@ -0,0 +1,41 @@
+package com.example.android_seminar_week3
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.android_seminar_week3.databinding.ActivitySignInBinding
+
+class SignInActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivitySignInBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySignInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        loginButton()
+        signInButton()
+    }
+
+    private fun loginButton(){
+        binding.btnLogin.setOnClickListener {
+            val id = binding.etId.text
+            val pw = binding.etPw.text
+            if(id.isEmpty() || pw.isEmpty()) {
+                Toast.makeText(this, "로그인 실패", Toast.LENGTH_SHORT).show()
+            }else{
+                val intent = Intent(this, HomeActivity::class.java)
+                startActivity(intent)
+            }
+        }
+    }
+
+    private fun signInButton(){
+        binding.btnSignUp.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
