0b38ab59c37c2de2a7232ad387b4a4a78e9449a6
hjh1161514 <hjh1161514@naver.com>
2021-10-31 18:24:22 +0900

    [UI] follower list, repository list

--- Android_Seminar_Week3/app/src/main/java/com/example/android_seminar_week3/RepositoryAdapter.kt
+++ Android_Seminar_Week3/app/src/main/java/com/example/android_seminar_week3/RepositoryAdapter.kt
@@ -1,24 +1,22 @@
 package com.example.android_seminar_week3
 
 import android.view.LayoutInflater
-import android.view.View.GONE
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.example.android_seminar_week3.databinding.ItemFollowerBinding
+import com.example.android_seminar_week3.databinding.ItemRepositoryBinding
 
 class RepositoryAdapter : RecyclerView.Adapter<RepositoryAdapter.RepositoryViewHolder>(){
     val repositoryList = mutableListOf<RepositoryData>()
 
-    class RepositoryViewHolder(private val binding: ItemFollowerBinding) : RecyclerView.ViewHolder(binding.root){
+    class RepositoryViewHolder(private val binding: ItemRepositoryBinding) : RecyclerView.ViewHolder(binding.root){
         fun onBind(data : RepositoryData){
-            binding.ivProfile.visibility = GONE
             binding.tvName.text = data.name
             binding.tvIntroduce.text = data.description
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RepositoryViewHolder {
-        val binding = ItemFollowerBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val binding = ItemRepositoryBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return RepositoryViewHolder(binding)
     }
 
